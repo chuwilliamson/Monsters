d5bcc7694c58b923e2696467a95c97ed6837a073
Ryan Bates <ryan.bates214@gmail.com>
2018-02-08 17:49:51 -0600

    work on character stats

--- Mad_Bash/Assets/Scripts/Ryan/Movement.cs
+++ Mad_Bash/Assets/Scripts/Ryan/Movement.cs
@@ -6,38 +6,47 @@ public class Movement : MonoBehaviour
 {
     public CharacterInfo character;
     public GameObject _camera;
+    public GameObject Door;
+    public Animator dooropener;
 
-    float horizontal;
-    float vertical;
+    float h;
+    float v;
 
 	// Use this for initialization
 	void Start ()
-    { 
+    {
         //transform.position = new Vector3(0, 0, 0);
+    }
+
+    public void DoorControl()
+    {
+        if (Input.GetButtonDown("Interact") && Vector3.Distance(transform.position, Door.transform.position) < 3)
+            dooropener.SetTrigger("open");
     }
 
     void Move()
     {
 
-        horizontal = Input.GetAxis("Horizontal") * character._speed;
-        vertical = Input.GetAxis("Vertical") * character._speed;
+        h = Input.GetAxis("Horizontal") * character._speed;
+        v = Input.GetAxis("Vertical") * character._speed;
 
-        horizontal *= Time.deltaTime;
-        vertical *= Time.deltaTime;
+        h *= Time.deltaTime;
+        v *= Time.deltaTime;
 
         Vector3 sprint = transform.forward;
 
-        if (Input.GetKey(KeyCode.LeftShift) && vertical >= .01f || (Input.GetKey(KeyCode.JoystickButton7) && vertical >= .01f))
+        if (Input.GetKey(KeyCode.LeftShift) && v >= .01f || (Input.GetKey(KeyCode.JoystickButton7) && v >= .01f))
         {
             transform.position += (sprint / 3);
         }
 
-        transform.Translate(horizontal, 0, vertical);
+        transform.Translate(h, 0, v);
     }
 
     // Update is called once per frame
     void Update ()
     {
         Move();
+        DoorControl();
     }
 }
