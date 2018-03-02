7603cbceefef6fee7a9641a26cf1129f3096bc61
Jeremy Barzas <jbarzas@gmail.com>
2018-03-02 13:23:19 -0600

    TestPlayerBehaviour implements dynamic callback

--- Mad_Bash/Assets/Scripts/Jeremy/TestPlayerBehaviour.cs
+++ Mad_Bash/Assets/Scripts/Jeremy/TestPlayerBehaviour.cs
@@ -4,11 +4,36 @@ using UnityEngine;
 
 public class TestPlayerBehaviour : MonoBehaviour
 {
+    public Container inventory;
     public int health;
+
+
+    public void TakeItem(Object obj)
+    {
+        inventory.AddContent(obj);
+    }
+
+    public void DropItem(Object obj)
+    {
+        inventory.RemoveContent(obj);
+    }
 
     public void TakeDamage()
     {
         health -= 10;
         Debug.Log("took damage");
+    }
+
+    public void TakeDamage(UnityEngine.Object[] args)
+    {
+        var sender = args[0];
+        var collidedwith = args[1];
+        if (collidedwith != gameObject)
+            return;
+        if(sender != null)
+        {
+            Debug.Log("taking damage from " + collidedwith.name);
+            TakeDamage();
+        }
     }
 }
