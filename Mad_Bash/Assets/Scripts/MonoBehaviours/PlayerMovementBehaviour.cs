23db5f9f0c1a5a877c834f29e1e5412cc05cd773
Jeremy Barzas <jbarzas@gmail.com>
2018-03-15 17:45:42 -0500

    current progress on player object prefab

--- Mad_Bash/Assets/Scripts/MonoBehaviours/PlayerMovementBehaviour.cs
+++ Mad_Bash/Assets/Scripts/MonoBehaviours/PlayerMovementBehaviour.cs
@@ -5,30 +5,45 @@ using UnityEngine;
 public class PlayerMovementBehaviour : MonoBehaviour
 {
     [SerializeField]
-    private float speed;
+    private float characterSpeed;    
+    public float speedModifer;
+    public GameObject _camera;
+    public GameObject _model;
 
-    void Update()
+    private void Update()
     {
-        Move();
-    }
+        // set speed modifer based on character speed stat
+        characterSpeed = GetComponent<PlayerObjectBehaviour>().CharacterInfo.Speed.Value;
 
-    void Move()
-    {
-        speed = GetComponent<PlayerObjectBehaviour>().CharacterInfo.Speed.Value;
-        float h = 0;
-        float v = 0;
+        // get input
+        float h = Input.GetAxis("LeftHorizontal");
+        float v = Input.GetAxis("LeftVertical");
 
-        h = Input.GetAxis("LeftHorizontal") * (speed * .5f);
-        v = Input.GetAxis("LeftVertical") * (speed * .5f);
+        // make input vector out of input
+        Vector3 input = new Vector3(h, 0, v) * Time.deltaTime;
+        Vector3 camForward = new Vector3(_camera.transform.forward.x, 0, _camera.transform.forward.z); ;
+        Vector3 translation = Vector3.zero;
 
-        h *= Time.deltaTime;
-        v *= Time.deltaTime;
+        // check to see if any input vector was given
+        if (input != Vector3.zero)
+        {
+            // add respect to camera's forward vector           
+            transform.forward = camForward;
 
-        float sprint = speed * .005f;
+            // apply modifiers to translation vector
+            translation = input * characterSpeed * speedModifer;            
 
-        transform.Translate(h, 0, v);
+            // apply translation vector to objects transform
+            transform.Translate(translation);
 
-        if (Input.GetButton("RightBumper") && v >= .01f)
-            transform.Translate(h, 0, (v + sprint));
+            // model must face in direction of the player objects translation vector            
+            _model.transform.forward = input.normalized;
+        }
+        Debug.Log("\n\n\n\n\n");
+        Debug.Log("Input Vector: " + input.normalized.ToString());
+        Debug.Log("Translation Vector: " + translation.normalized.ToString());
+        Debug.Log("Model Forward: " + _model.transform.forward.ToString());        
+        Debug.Log("Camera Forward: " + camForward.ToString());
+        Debug.Log("\n\n\n\n\n");
     }
 }
