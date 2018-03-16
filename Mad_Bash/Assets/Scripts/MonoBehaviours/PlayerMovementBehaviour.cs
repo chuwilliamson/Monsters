9ff481230845ae19b6fe869e7c6d2809a6d57ae5
Jeremy Barzas <jbarzas@gmail.com>
2018-03-16 14:25:47 -0500

    current progress

--- Mad_Bash/Assets/Scripts/MonoBehaviours/PlayerMovementBehaviour.cs
+++ Mad_Bash/Assets/Scripts/MonoBehaviours/PlayerMovementBehaviour.cs
@@ -12,47 +12,45 @@ public class PlayerMovementBehaviour : MonoBehaviour
     private StringVariable yAxis;
 
     [SerializeField]
-    private float characterSpeedLevel;    
-    public float speedModifer;
     public GameObject _camera;
+    [SerializeField]
     public GameObject _model;
+
+    [SerializeField]
+    private float characterSpeedLevel;
+    [SerializeField]
+    public float speedModifer;    
 
     private void Update()
     {
-        // get input        
-        float y = Input.GetAxis(yAxis.Value);
-        float x = Input.GetAxis(xAxis.Value);
+        // get characters speed level
+        characterSpeedLevel = GetComponent<PlayerObjectBehaviour>().CharacterInfo.Speed.Value;
 
-        // make input vector with respect to delta time     
-        Vector3 input = new Vector3(y, 0, x) * Time.deltaTime;
-        
+        MovePlayer();
+    }
+
+    public void MovePlayer()
+    {
+        // get input vector with respect to delta time
+        float x = Input.GetAxis(xAxis.Value); // horizontal
+        float y = Input.GetAxis(yAxis.Value); // vertical
+        Vector3 input = new Vector3(x, 0, y) * Time.deltaTime;
+
         // check for 0 input
-        if (input != Vector3.zero)
-        {
-            // get characters speed level
-            characterSpeedLevel = GetComponent<PlayerObjectBehaviour>().CharacterInfo.Speed.Value;
+        if (input == Vector3.zero)
+            return;
 
-            // get camera's forward vector
-            Vector3 camForward = new Vector3(_camera.transform.forward.x, 0, _camera.transform.forward.z);
+        Debug.Log("Input given");
+        Debug.Log(input.ToString());
 
-            // calculate translation direction with respect to camera's forward
-            Vector3 translation = (input + camForward).normalized;
+        // get camera's forward vector
+        Vector3 camForward = new Vector3(_camera.transform.forward.x, 0, _camera.transform.forward.z);
 
-            Debug.Log("\n\n\n\n\n");
-            Debug.Log("Input Vector: " + input.normalized.ToString());
-            Debug.Log("Camera Forward: " + camForward.ToString());            
-            Debug.Log("Translation Vector Direction: " + translation.ToString());
-            
-            // calculate coeffiect to use to modify translation vector
-            float translationModifer = characterSpeedLevel * speedModifer;
+        // calculate translation direction with respect to camera's forward
+        Vector3 translation = input - camForward;
+        Debug.Log(translation.ToString());
 
-            // apply translation vector to objects transform
-            transform.Translate(translation * translationModifer);
-
-            // model must face in direction of the player objects translation vector            
-            _model.transform.forward = input.normalized;
-            Debug.Log("Model Forward: " + _model.transform.forward.ToString());
-            Debug.Log("\n\n\n\n\n");
-        }
+        // apply translation vector to objects transform
+        transform.Translate(translation);
     }
 }
