46503b90fee7aa1251b23db75b3a4faa1f1f8c4e
Ryan Bates <ryan.bates214@gmail.com>
2018-03-21 13:49:54 -0500

    made Input Manager Changes

---
+++ Mad_Bash/Assets/Scripts/MonoBehaviours/HealthSliderBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HealthSliderBehaviour : MonoBehaviour
+{
+    public Slider Health;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        Health.minValue = 0.0f;
+        Health.maxValue = GetComponent<CharacterInformation>().Health.Value;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    Health.value = GetComponent<CharacterInformation>().Health.Value;
+    }
+}
