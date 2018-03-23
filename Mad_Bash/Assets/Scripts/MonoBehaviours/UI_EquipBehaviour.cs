3c1a7b80c4efc3e23c86e823af51f85c4013bb89
Ryan Bates <ryan.bates214@gmail.com>
2018-03-23 17:54:53 -0500

    this is the begining of the inventory ui
    pause and core gameplay with health and fear gauges are done

---
+++ Mad_Bash/Assets/Scripts/MonoBehaviours/UI_EquipBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_EquipBehaviour : MonoBehaviour
+{
+    bool equipped = false;
+
+    public void Equip()
+    {
+        if (Input.GetButton("A Button"))
+        {
+            equipped = true;
+        }
+
+        if (equipped == true)
+        {
+            Instantiate(gameObject, gameObject.transform);
+        }
+    }
+}
