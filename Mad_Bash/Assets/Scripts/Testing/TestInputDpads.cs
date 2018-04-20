b022a3e5bc5495b07c1075831198f52412df7b47
Matthew Williamson <chuwilliamson@gmail.com>
2018-04-20 11:44:19 -0500

    testinput dpad
    testinput behaviour impl

---
+++ Mad_Bash/Assets/Scripts/Testing/TestInputDpads.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInputDpads : MonoBehaviour
+{
+    public StringVariable DpadHorizontal;
+
+    public StringVariable DpadVertical;
+
+    public Vector2 DpadInput;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    DpadInput = new Vector2(Input.GetAxis(DpadHorizontal.Value), Input.GetAxis(DpadVertical.Value));
+	}
+}
