ca8bcf386f9873148b4df1f5f75b6aaf6998c5eb
Matthew Williamson <chuwilliamson@gmail.com>
2018-04-27 15:41:27 -0500

    integrated animations

---
+++ Mad_Bash/Assets/Models/Alyssa_Taylor/Scripts/AlyssaTaylorAnimatorBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlyssaTaylorAnimatorBehaviour : MonoBehaviour, IInteractionBeginHandler
+{
+
+    public static readonly int SPEED = Animator.StringToHash("Speed");
+    public static readonly int ATTACK = Animator.StringToHash("Attack");
+    public static readonly int OPEN = Animator.StringToHash("Open");
+    public static readonly int INTERACT = Animator.StringToHash("Interact");
+    public static readonly int HEALTH = Animator.StringToHash("Health");
+    [SerializeField]
+    private Animator _anim;
+    [SerializeField]
+    private FloatVariable _Speed;
+    [SerializeField]
+    private FloatVariable _CurrentSpeed;
+    [SerializeField]
+    private FloatVariable _Health;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _anim.SetFloat(SPEED, _CurrentSpeed.Value/_Speed.Value);
+        _anim.SetFloat(HEALTH, _Health.Value);
+    }
+    
+    public void OnInteractionBegin(Object[] args)
+    {
+        _anim.SetTrigger(INTERACT);
+    }
+}
