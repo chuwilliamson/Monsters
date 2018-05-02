6c53087e0842fc493963d2504cbf23bece59e84a
Matthew Williamson <chuwilliamson@gmail.com>
2018-05-02 12:15:18 -0500

    make classes

---
+++ Mad_Bash/Assets/Import/FightOrFlight/Scripts/PlayerContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContext : IContext
+{
+
+    public void UpdateContext()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ChangeState(IState next)
+    {
+        throw new System.NotImplementedException();
+    }
+}
