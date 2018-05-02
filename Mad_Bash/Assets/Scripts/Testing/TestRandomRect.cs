c3fce8a77b9583247fdfd94fe304f12451c8e0e8
Jeremy Barzas <jbarzas@gmail.com>
2018-05-02 16:32:08 -0500

    current progress in my own scene

---
+++ Mad_Bash/Assets/Scripts/Testing/TestRandomRect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRandomRect : MonoBehaviour
+{
+    public Canvas canvas;
+
+    private void OnEnable()
+    {
+        canvas = GetComponentInParent<Canvas>();
+    }
+
+    int counter;
+    private void Update()
+    {
+        counter++;
+        if (counter > 10)
+        {
+            var width = canvas.GetComponent<RectTransform>().rect.width;
+            var height = canvas.GetComponent<RectTransform>().rect.height;
+
+            MoveObject(width, height);
+            counter = 0;
+        }        
+    }
+
+    public void MoveObject(float width, float height)
+    {
+        var newPos = RandomRectPos.RandomOnRect(width, height);
+        gameObject.transform.position = newPos;
+    }      
+}
