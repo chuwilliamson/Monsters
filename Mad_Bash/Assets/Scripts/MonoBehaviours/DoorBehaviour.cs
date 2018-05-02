9555b933e8e6b89c8b0b93f065861c37d6163230
Ryan Bates <rbates214@gmail.com>
2018-05-02 13:34:30 -0500

    have to go and make each event its own argument

--- Mad_Bash/Assets/Scripts/MonoBehaviours/DoorBehaviour.cs
+++ Mad_Bash/Assets/Scripts/MonoBehaviours/DoorBehaviour.cs
@@ -2,20 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator), typeof(GameEventArgsListener))]
+[DisallowMultipleComponent]
 public class DoorBehaviour : MonoBehaviour
 {
-
+    ButtonPressContext _buttonPressContext;
+    public string DoorOpen = "open";
     private Animator doorAnim;
+
+    public void OnContextFinished(Object[] args)
+    {
+        if (_buttonPressContext.TotalScore > 3)
+        {
+            doorAnim.SetTrigger(DoorOpen);
+        }
+    }
     
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-        doorAnim = GetComponent<Animator>();	
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        if()
-	}
+        doorAnim = GetComponent<Animator>();
+    }
 }
