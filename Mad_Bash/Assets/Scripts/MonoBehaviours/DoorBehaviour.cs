ba66ddf81158fe41cf6fc23071cdde10754ade66
Ryan Bates <rbates214@gmail.com>
2018-05-02 12:55:49 -0500

    made a door script and made a door prefab

---
+++ Mad_Bash/Assets/Scripts/MonoBehaviours/DoorBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour
+{
+
+    private Animator doorAnim;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        doorAnim = GetComponent<Animator>();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+}
