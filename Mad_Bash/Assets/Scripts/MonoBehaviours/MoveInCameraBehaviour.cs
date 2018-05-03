3b6e836f4d0552786733d856a6c7e72ccd4bc0c7
Jeremy Barzas <jbarzas@gmail.com>
2018-05-03 17:05:50 -0500

    stuff works

---
+++ Mad_Bash/Assets/Scripts/MonoBehaviours/MoveInCameraBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveInCameraBehaviour: MonoBehaviour
+{
+    public Camera cam;
+
+    public Vector3 MoveInCamera()
+    {
+        float newX = Random.Range(0, cam.pixelWidth);
+        float newY = Random.Range(0, cam.pixelHeight);
+        
+        return cam.ScreenToWorldPoint(new Vector3(newX, newY, 15));
+    }
+
+    int counter;
+	void Update ()
+    {
+        counter++;
+        if (counter > 10)
+        {
+            gameObject.transform.SetPositionAndRotation(MoveInCamera(), cam.transform.rotation);
+            counter = 0;
+        }
+	}
+}
