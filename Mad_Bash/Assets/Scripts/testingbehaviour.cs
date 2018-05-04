25d887c60b96f9409b46b7d9781da82898c02603
Jeremy Barzas <jbarzas@gmail.com>
2018-05-04 17:32:09 -0500

    Added script for testing anything "this is person stuff"

---
+++ Mad_Bash/Assets/Scripts/testingbehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingBehaviour : MonoBehaviour
+{
+    private void Update()
+    {
+        Debug.Log(Random.Range(0.0f, 1.0f));
+    }
+}
