1277975bde27fbdf7fcf65bc0e9b4a21ab087512
Jeremy Barzas <jbarzas@gmail.com>
2018-05-04 16:30:52 -0500

    Modified MoveInCaera method

--- Mad_Bash/Assets/Import/GameEventTrigger/Helpers.cs
+++ Mad_Bash/Assets/Import/GameEventTrigger/Helpers.cs
@@ -27,17 +27,17 @@ public static class ExtensionMethods
         gameObject.transform.localPosition = RandomRectPos.RandomOnRect(width, height);
     }
 
-    public static void MoveInCamera(this GameObject gameObject, Vector3 newPos, bool local = false)
+    public static void MoveInCamera(this GameObject gameObject, Vector3 newPos)
     {
-        var screenSpace = Camera.main.WorldToScreenPoint(newPos);
-        if (local)
-        {
-            gameObject.transform.localPosition = Camera.main.ScreenToViewportPoint(screenSpace);
-        }
-        else
-        {
-            gameObject.transform.position = Camera.main.ScreenToViewportPoint(screenSpace);
-        }
-        
+        gameObject.transform.position = newPos;
+        var pos = Camera.main.WorldToViewportPoint(gameObject.transform.position);
+        pos.x = Mathf.Clamp(pos.x, 0.2f, 0.8f);
+        pos.y = Mathf.Clamp(pos.y, 0.2f, 0.8f);
+        pos.z = Mathf.Clamp(pos.z, 3, 6);
+        gameObject.transform.position = Camera.main.ViewportToWorldPoint(pos);
+
+        //var viewport = Camera.main.WorldToViewportPoint(gameObject.transform.position);
+        //var worldpoint = Camera.main.ViewportToWorldPoint(viewport);        
+        //gameObject.transform.position = worldpoint;
     }
 }
