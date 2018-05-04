58193b45b3ced7ac5af1b560dd4dc21b071a0a87
Matthew Williamson <chuwilliamson@gmail.com>
2018-05-04 12:39:55 -0500

    change the range

--- Mad_Bash/Assets/Scripts/RandomIdleStateBehaviour.cs
+++ Mad_Bash/Assets/Scripts/RandomIdleStateBehaviour.cs
@@ -3,33 +3,11 @@
 public class RandomIdleStateBehaviour : StateMachineBehaviour
 {
     public static readonly int IDLEINDEX = Animator.StringToHash("IdleIndex");
-
-    public int randomIndex;
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        randomIndex = Random.Range(0, 4);
-    }
-
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-
-    }
-
+    
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.SetInteger(IDLEINDEX, randomIndex);
+        animator.SetInteger(IDLEINDEX, Random.Range(0, 3));
     }
 
-    // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
-    public override void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-    }
-
-    // OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
-    public override void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-    }
 }
