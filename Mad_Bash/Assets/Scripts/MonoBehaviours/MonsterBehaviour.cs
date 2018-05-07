f1669b079120ed608f86e6d23f78e184e3b2e90a
Matthew Williamson <chuwilliamson@gmail.com>
2018-05-07 12:32:39 -0500

    build is up

--- Mad_Bash/Assets/Scripts/MonoBehaviours/MonsterBehaviour.cs
+++ Mad_Bash/Assets/Scripts/MonoBehaviours/MonsterBehaviour.cs
@@ -2,29 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterBehaviour : MonoBehaviour
+public class MonsterBehaviour : ContextResolutionBehaviour
 {
-    
-    [SerializeField]
-    ButtonPressContext _buttonPressContext;
-
-    public GameEventArgsResponse WinResponse;
-    public GameEventArgsResponse LoseResponse;
-    private Animator _anim;
-
-    public void Start()
-    {
-        _anim = GetComponent<Animator>();
-    }
-    public void OnContextFinished(Object[] args)
-    {
-        if (_buttonPressContext.TotalScore >= 3)
-            WinResponse.Invoke(args);
-        else
-        {
-            LoseResponse.Invoke(args);
-        }
-    }
-
     
 }
