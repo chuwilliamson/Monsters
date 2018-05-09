7f3cb53f3ef1616618f5040a282a4564f9595bbb
Ryan Bates <rbates214@gmail.com>
2018-05-09 17:57:38 -0500

    made a new scene for losing to the goblin
    have it restarting the sceen

---
+++ Mad_Bash/Assets/Scripts/MonoBehaviours/SceneManagerBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerBehaviour : MonoBehaviour
+{
+    public Camera mainCam;
+
+    public float fadeTime = 1.0f;
+    public float alpha = 0f; // color of the screen
+
+    public GameObject plane;
+
+    public void ChangeScene()
+    {
+        Instantiate<GameObject>(plane, mainCam.transform.position, mainCam.transform.rotation, mainCam.transform);
+
+        this.StartCoroutine(WaitAndLoad());
+    }
+
+    IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene(0);
+    }
+}
