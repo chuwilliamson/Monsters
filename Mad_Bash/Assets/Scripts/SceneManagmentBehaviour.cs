b228a72a529c6a13cd637655e65c6148042abea4
Ryan Bates <rbates214@gmail.com>
2018-05-09 17:19:03 -0500

    waits to change back to the scene
    started working on the screen fade to black and fade back in

---
+++ Mad_Bash/Assets/Scripts/SceneManagmentBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagmentBehaviour : MonoBehaviour
+{
+    public float fadeTime = 1.0f;
+    public float alpha = 0f; // color of the screen
+
+    Texture2D black;
+    Rect Screen;
+
+    public void ChangeScene()
+    {
+        this.StartCoroutine(WaitAndLoad());
+        OnGUI();
+    }
+    
+    void OnGUI()
+    {
+        Screen = new Rect(0, 0, Screen.width, Screen.height);
+
+        GUI.DrawTexture(Screen, black);
+    }
+    
+    IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene(0);
+    }
+}
