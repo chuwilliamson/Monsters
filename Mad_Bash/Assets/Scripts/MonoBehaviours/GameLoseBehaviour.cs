75d98962ebeff7ac1525707bb8f3a4e66fb5b5c0
Jeremy Barzas <jbarzas@gmail.com>
2018-05-11 13:39:40 -0500

    game reset changes

---
+++ Mad_Bash/Assets/Scripts/MonoBehaviours/GameLoseBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLoseBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private GameEventArgs GameLose;
+
+    public void GameOver()
+    {
+        GameLose.Raise();
+    }
+}
